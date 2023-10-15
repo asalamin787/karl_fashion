1809a6b23b53278bb2f4c23683272e3d7e304c34
Alamin Sikder <117014662+asalamin787@users.noreply.github.com>
2023-10-16 00:37:10 +0600

    resources update

--- app/Http/Controllers/category/categoryController.php
+++ app/Http/Controllers/category/categoryController.php
@@ -9,26 +9,17 @@ use Illuminate\Http\Request;
 
 class categoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $categorys=Category::latest()->get();
         return view('dashboard.category.show_category', compact('categorys'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('dashboard.category.create_category');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {   
         $request->validate([
@@ -44,25 +35,16 @@ class categoryController extends Controller
         return redirect('/categories')->with('message', 'Register Success');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show()
     {
        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Category $category)
     {
         return view('dashboard.category.edit_category', compact('category'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Category $category)
     {   
         $category->update([
@@ -72,9 +54,6 @@ class categoryController extends Controller
         return redirect('/categories');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Category $category)
     {
         $category->delete();
